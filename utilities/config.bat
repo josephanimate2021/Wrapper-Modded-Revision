d4d8db9e56ce68bb57ee1c570c4daffef5dde74f
Joseph The Animator <100003036+josephanimate2021@users.noreply.github.com>
2022-04-30 17:40:46 -0400

    Added The 2020 Google Chrome

--- utilities/config.bat
+++ utilities/config.bat
@@ -8,10 +8,10 @@ if "%SUBSCRIPT%"=="" ( start notepad.exe config.bat & exit )
 endlocal
 
 :: Shows exactly Offline is doing, and never clears the screen. Useful for development and troubleshooting. Default: n
-set VERBOSEWRAPPER=n
+set VERBOSEWRAPPER=y
 
 :: Won't check for dependencies (flash, node, etc) and goes straight to launching. Useful for speedy launching post-install. Default: n
-set SKIPCHECKDEPENDS=n
+set SKIPCHECKDEPENDS=y
 
 :: Won't install dependencies, regardless of check results. Overridden by SKIPCHECKDEPENDS. Mostly useless, why did I add this again? Default: n
 set SKIPDEPENDINSTALL=n
