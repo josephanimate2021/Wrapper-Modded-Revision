d7f8de147b06bd606ac4663d28e2ba7e78c8d481
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-05-06 15:47:57 -0400

    Update load.js

--- wrapper/movie/load.js
+++ wrapper/movie/load.js
@@ -27,24 +27,40 @@ module.exports = function (req, res, url) {
 		}
 
 		case "POST": {
-			if (!url.path.startsWith("/goapi/getMovie/")) return;
-			res.setHeader("Content-Type", "application/zip");
-			process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0";
+			switch (url.pathname) {
+				case "/goapi/getMovie/": {
+					res.setHeader("Content-Type", "application/zip");
+					process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0";
 
-			movie // try movie
-				.loadZip(url.query.movieId)
-				.then(b => res.end(Buffer.concat([base, b])))
-				.catch(err => {
-					starter // try starter
-						.load(url.query.movieId)
+					movie // try movie
+						.loadZip(url.query.movieId)
 						.then(b => res.end(Buffer.concat([base, b])))
-						.catch(err => { // error
+						.catch(err => {
+						starter // try starter
+							.load(url.query.movieId)
+							.then(b => res.end(Buffer.concat([base, b])))
+							.catch(err => { // error
 							if (process.env.NODE_ENV == "dev") throw err;
 							console.error("Error loading movie: " + err)
 							res.end("1")
 						});
-				});
-			return true;
+					});
+					return true;
+				}
+				case "/ajax/deleteMovie/": {
+					res.setHeader("Content-Type", "application/zip");
+					process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0";
+
+					movie // try movie
+						.delete(url.query.movieId)
+						.catch(err => { // error
+						if (process.env.NODE_ENV == "dev") throw err;
+						console.error("Error deleting movie: " + err)
+						res.end("1")
+					});
+					return true;
+				}
+			}
 		}
 		default: return;
 	}
