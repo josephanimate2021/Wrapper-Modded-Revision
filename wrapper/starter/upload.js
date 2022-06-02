dc45d95514506055a9b766f922a27f3256e82b95
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-06-02 12:43:21 -0400

    Update upload.js

--- wrapper/starter/upload.js
+++ wrapper/starter/upload.js
@@ -1,24 +1,15 @@
 /***
  * starter upload route (in development)
  */
-const starter = require("./main");
-const loadPost = require("../request/post_body");
-
 function createId() {
 	return Math.random().toString(16).substring(2, 9);
 }
 
 module.exports = function (req, res, url) {
-	if (req.method != "POST") {
-		switch (url.path) {
-			case "/upload_starter": {
-				const id = createId();
-				const url = `https://josephanimate2021.github.io/Vyond-Legacy-Offline/utilities/offline-redirect-helper/starterUploaded?returnMessage=Your Starter Could Not Be Uploaded! Id: ${id}`;
-				res.statusCode = 302;
-				res.setHeader("Location", url);
-				res.end();
-			}
-		}
-	}
-	return true;
+	if (req.method != "POST" || url.path != "/upload_starter") return;
+	const id = createId();
+	const url = `https://josephanimate2021.github.io/Vyond-Legacy-Offline/utilities/offline-redirect-helper/starterUploaded?returnMessage=Your Starter Could Not Be Uploaded! Id: ${id}`;
+	res.statusCode = 302;
+	res.setHeader("Location", url);
+	res.end();
 }
