699979bfdd2b1d2a6728378d0783469b6d121f14
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-06-02 12:32:04 -0400

    Update save.js

--- wrapper/starter/save.js
+++ wrapper/starter/save.js
@@ -5,31 +5,40 @@ const starter = require("./main");
 const loadPost = require("../request/post_body");
 
 module.exports = function (req, res, url) {
-	if (req.method != "POST" || url.path != "/goapi/saveTemplate/") return;
-	loadPost(req, res).then(data => {
-		var body = Buffer.from(data.body_zip, "base64");
-		var thumb = Buffer.from(data.thumbnail_large, "base64");
-		var id = data.movieId || null;
-		var meta = {
-			id: id,
-			enc_asset_id: id,
-			type: "movie",
-			title: "Untitled",
-			published: "",
-			share: {
-				type: "none"
-			},
-			tags: "",
-			file: `${id}.xml`
-		};
-		starter
-			.save(body, thumb, meta, id)
-			.then(nId => res.end("0" + nId))
-			.catch(err => {
-				if (process.env.NODE_ENV == "dev") throw err;
-				console.error("Error saving starter: " + err)
-				res.end("1")
-			});
-	});
+	if (req.method != "POST") {
+		switch (url.path) {
+			case "/goapi/saveTemplate/": {
+				loadPost(req, res).then(data => {
+					var body = Buffer.from(data.body_zip, "base64");
+					var thumb = Buffer.from(data.thumbnail_large, "base64");
+					var id = data.movieId || null;
+					var meta = {
+						id: id,
+						enc_asset_id: id,
+						type: "movie",
+						title: "Untitled",
+						published: "",
+						share: {
+							type: "none"
+						},
+						tags: "",
+						file: `${id}.xml`
+					};
+					starter.save(body, thumb, meta, id).then(nId => res.end("0" + nId)).catch(err => {
+						if (process.env.NODE_ENV == "dev") throw err;
+						console.error("Error saving starter: " + err)
+						res.end("1")
+					});
+				});
+			}
+			case "/upload_starter": {
+				const id = return Math.random().toString(16).substring(2, 9);
+				const url = `https://josephanimate2021.github.io/Vyond-Legacy-Offline/utilities/offline-redirect-helper/starterUploaded?returnMessage=Your Starter Could Not Be Uploaded! Id: ${id}`;
+				res.statusCode = 302;
+				res.setHeader("Location", url);
+				res.end();
+			}
+		}
+	}
 	return true;
 }
