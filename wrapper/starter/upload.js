7e29f49e8308e107e37571e02b9d47c1e801c438
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-06-02 00:46:48 -0400

    Update upload.js

--- wrapper/starter/upload.js
+++ wrapper/starter/upload.js
@@ -4,14 +4,16 @@
 const formidable = require("formidable");
 const fs = require("fs");
 const starter = require("./main");
+const fUtil = require("../fileUtil");
 
 module.exports = function (req, res, url) {
 	if (req.method != "POST" || url.path != "/upload_starter") return;
 	new formidable.IncomingForm().parse(req, (e, f, files) => {
 		const path = files.import.path, buffer = fs.readFileSync(path);
-		var id = req.body.movieId || null;
-		var body = Buffer.from(req.body.body_zip, "base64");
-		var thumb = Buffer.from(req.body.thumbnail_large, "base64");
+		var id = fUtil.generateId();
+		var data = "req.body";
+		var body = Buffer.from(data.body_zip, "base64");
+		var thumb = Buffer.from(data.thumbnail_large, "base64");
 		const meta = {
 			id: id,
 			enc_asset_id: id,
@@ -26,7 +28,7 @@ module.exports = function (req, res, url) {
 		};
 		try {
 			// save the starter
-			const id = char.save(buffer, body, thumb, meta, id);
+			const id = starter.save(buffer, body, thumb, meta, id);
 			const url = `https://josephanimate2021.github.io/Vyond-Legacy-Offline/utilities/offline-redirect-helper/starterUploaded?returnMessage=Your Starter Has Been Uploaded! Id: ${id}`;
 			fs.unlinkSync(path);
 			// redirect the user
@@ -34,7 +36,7 @@ module.exports = function (req, res, url) {
 			res.setHeader("Location", url);
 			res.end();
 		} catch (err) {
-			console.error("Error uploading starter: " + err);
+			res.setHeader("Location", "https://josephanimate2021.github.io/Vyond-Legacy-Offline/utilities/offline-redirect-helper/starterUploaded?returnMessage=Your Starter Could Not Be Uploaded! Please contact joseph the animator hashtag 2292 on discord to get this issue resolved.");
 			res.statusCode = 500;
 			res.end("00");
 		}
