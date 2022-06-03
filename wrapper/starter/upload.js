963a7c1b83472fb56ed6edd2fa8d50c65d99e0b2
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-06-03 12:44:09 -0400

    Update upload.js

--- wrapper/starter/upload.js
+++ wrapper/starter/upload.js
@@ -7,9 +7,7 @@ function createId() {
 
 module.exports = function (req, res, url) {
 	if (req.method != "POST" || url.path != "/upload_starter") return;
-	const id = createId();
-	const url = `https://josephanimate2021.github.io/Vyond-Legacy-Offline/utilities/offline-redirect-helper/starterUploadFailed`;
 	res.statusCode = 302;
-	res.setHeader("Location", url);
+	res.setHeader("Location", "https://josephanimate2021.github.io/Vyond-Legacy-Offline/utilities/offline-redirect-helper/starterUploadFailed");
 	res.end();
 }
