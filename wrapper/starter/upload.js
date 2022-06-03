4de69f9882a7ad2daa25a42ae60d4e3117c00616
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-06-03 12:44:32 -0400

    Update upload.js

--- wrapper/starter/upload.js
+++ wrapper/starter/upload.js
@@ -1,10 +1,6 @@
 /***
  * starter upload route (in development)
  */
-function createId() {
-	return Math.random().toString(16).substring(2, 9);
-}
-
 module.exports = function (req, res, url) {
 	if (req.method != "POST" || url.path != "/upload_starter") return;
 	res.statusCode = 302;
